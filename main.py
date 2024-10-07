b60e73c3895bccb4403cb1b9000a6001fa3cda48
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2024-10-07 15:59:25 +0800

    Fixed CORS issue from sources other from mobile request

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import subprocess
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import spacy
 import uvicorn
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 try:
     nlp = spacy.load("en_core_web_sm")
