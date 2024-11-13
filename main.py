c5a3c17cbb16ef899a380aa5b65312a82f318c94
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2024-11-13 08:27:39 +0800

    corrected article

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ def generate_sentence(input_words):
             if obj.endswith('s'):
                 sentence += f", {subject.capitalize()} want {obj}"
             else:
-                article += "an" if obj[0].lower() in "aeiou" else "a"
+                article = "an" if obj[0].lower() in "aeiou" else "a"
                 sentence += f", {subject.capitalize()} want {article} {obj}"
         elif verb:
             sentence += f", {subject.capitalize()} want to {verb}"
